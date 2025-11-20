b581de0e4fc83e5b1a23da624ea9ed54754664e1
Paul Barron <paulinobarron85@outlook.com>
2025-11-20 13:50:27 -0800

    Basic functionality and layout complete

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var page = document.getElementsByClassName("page");
 
+var allAnimate = document.getElementsByClassName("");
+
 function changePage(index) {
     for(var i = 0; i < page.length; i++) {
         page[i].style.display = "none";
@@ -7,5 +9,11 @@ function changePage(index) {
     page[index - 1].style.display = "block";
     window.scroll(0, 0);
 }
+
+function onScroll() {
+    
+}
+
+window.scrollTo(0, 0);
 
 changePage(1);
